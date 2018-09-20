7192bebffe1002768725c24c7052bbb96dc88114
nsom <somavarapun@gmail.com>
2018-09-20 20:44:33 +0000

    refactored datagen.

--- cocoloader.py
+++ cocoloader.py
@@ -1,170 +1,209 @@
 import torch
+import cv2
 import numpy as np 
 from torch.utils.data import Dataset, DataLoader
-from skimage import io, transform
 from torchvision import transforms, utils
 import glob
 import os
 import json
 
-img_ext = ''
+img_ext = '.jpg'
 ann_ext = '.json'
 limb_set = [(0,1), (0,2), (0,3), (2,4), (3,5), (1,6), (1,7), (6,8), (7,9), (8,10), (9,11), (1,12), (1,13), (12,14), (13,15), (14,16), (15,17)]
 
-def gaussianOnPt(conf_map_total, point, sigma):
+def put_gaussian(point, gauss_acc, sigma, stride):
+    start = stride / 2.0 - 0.5
     
-    tmp_map = np.zeros((conf_map_total.shape[0], conf_map_total.shape[1]))
-    gridx = np.linspace(-int(sigma * 2), int(sigma * 2), sigma * 4 + 1)
-    gridy = np.linspace(-int(sigma * 2), int(sigma * 2), sigma * 4 + 1)
-    x_vals, y_vals = np.meshgrid(gridx, gridy)
+    x_grid, y_grid = np.meshgrid(np.arange(gauss_acc.shape[0]), np.arange(gauss_acc.shape[1]))
+
+    x_grid = x_grid * stride + start
+    y_grid = y_grid * stride + start
+
+    dist = (np.power((x_grid - point[0]),2) +  np.power((y_grid - point[1]),2))/(2.0 * np.power(sigma, 2))
+    dist[dist > 4.6052] = 0
+    dist = np.exp(-dist)
+
+    gauss_acc = np.max(np.dstack((gauss_acc, dist)), axis=2)
+    gauss_acc[gauss_acc > 1] = 1
     
-    grid = np.exp(- (np.sqrt(np.power(x_vals, 2) + np.power(y_vals, 2)))/ np.power(sigma, 2))
+    return gauss_acc
+
+def put_paf(point1, point2, paf_acc, theta, stride):
+    point1 = np.array(point1[:2], dtype=np.float) / stride
+    point2 = np.array(point2[:2], dtype=np.float) / stride
+
+    vec = point2 - point1
+    vec_norm = np.linalg.norm(vec)
     
-    tmp_map[point[1]-int(sigma * 2):point[1]+int(sigma * 2)+1, point[0]-int(sigma * 2):point[0]+int(sigma * 2)+1] = grid
+    if vec_norm == 0:
+        return paf_acc
 
-    conf_map_total = np.maximum(conf_map_total, tmp_map)
-    conf_map_total[conf_map_total > 1] = 1
+    vec_unit = vec/vec_norm
+    vec_perp_unit = np.array([vec[1], -vec[0]])/vec_norm
+    
+    minx = max(int(round(min(point1[0], point2[0]) - theta)), 0)
+    maxx = min(int(round(max(point1[0], point2[0]) + theta)), paf_acc.shape[1] - 1)
+    miny = max(int(round(min(point1[1], point2[1]) - theta)), 0)
+    maxy = min(int(round(min(point1[1], point2[1]) + theta)), paf_acc.shape[0] - 1)
 
-    return conf_map_total
+    x_grid, y_grid = np.meshgrid(np.linspace(minx, maxx, maxx-minx+1), np.linspace(miny, maxy, maxy-miny+1))
 
-def pafOnPt(paf_total, paf_counts, point1, point2, sigma):
-    paf_vec = point2 - point1
-    paf_tmp = np.zeros(paf_total.shape)
+    x_grid -= point1[0]
+    y_grid -= point1[1]
 
-    limb_norm = np.sqrt(np.sum(np.power(paf_vec, 2), axis=0))
+    p_test = x_grid * vec_perp_unit[0] + y_grid * vec_perp_unit[1]
+    p_test[p_test > theta] = 0
 
-    if limb_norm != 0:
-        v = paf_vec/limb_norm
-        v_perp = np.array([v[1], -v[0]])
+    new_vec_map_0 = np.zeros((p_test.shape[0], p_test.shape[1]))
+    new_vec_map_1 = np.zeros((p_test.shape[0], p_test.shape[1]))
 
-        x1 = 0
-        x2 = paf_total.shape[1]
-        y1 = 0
-        y2 = paf_total.shape[0]
-        grid_x = np.linspace(x1, x2 - 1, x2 - x1)
-        grid_y = np.linspace(y1, y2 - 1, y2 - y1)
+    new_vec_map_0[p_test != 0] = 1
+    new_vec_map_1[p_test != 0] = 1
 
-        x_vals, y_vals = np.meshgrid(grid_x, grid_y)
+    num_p = len(p_test != 0)
 
-        x_vals = x_vals - point1[0]
-        y_vals = y_vals - point1[1]
+    paf_curr = np.dstack((new_vec_map_0 * vec_unit[0], new_vec_map_1 * vec_unit[1]))
 
-        x_vals_1 = x_vals * v[0]
-        y_vals_1 = y_vals * v[1]
-        total_v = x_vals_1 + y_vals_1
+    tmp_map = np.zeros(paf_acc.shape)
+    tmp_map[miny:maxy+1, minx:maxx+1] = paf_curr
 
-        x_vals_2 = x_vals * v_perp[0]
-        y_vals_2 = y_vals * v_perp[1]
-        total_v_perp = np.abs(x_vals_2 + y_vals_2)
-        logical_idx = (total_v >= 0) & (total_v <= limb_norm) & (total_v_perp < sigma)
+    return (paf_acc + tmp_map), num_p
 
-        paf_tmp[:,:,0][logical_idx] = v[0]
-        paf_tmp[:,:,1][logical_idx] = v[1]
 
-        paf_total[:,:,0] += paf_tmp[:,:,0]
-        paf_total[:,:,1] += paf_tmp[:,:,1]
-        paf_counts[paf_tmp[:,:,0] > 0] += 1
-        
-    return paf_total
+class CocoPoseDataset:
 
-class CocoPoseDataset(Dataset):
+    def __init__(self, ann_dir, img_dir, size=(368, 368), end_size=(46,46), theta=1.0, sigma=7, stride=8):
 
-    def __init__(self, img_dir, ann_dir, transforms=None):
-        kps = glob.glob(os.path.join(ann_dir, '*' + ann_ext))
-        imgs = glob.glob(os.path.join(img_dir, '*' + img_ext))
-        self.data = []
-        self.transforms = transforms
+        print('Loading Datasets...')
 
-        print('Loading dataset paths...')
-        for img in imgs:
-            fname = img.split('/')[-1]
-            ext = fname.split('.')[-1]
-            kps_fname = os.path.join(ann_dir, fname.replace('.' + ext, ann_ext))
-            
-            if kps_fname in kps:
-                self.data.append((img, kps_fname))
-        
+        ann_pths = glob.glob(os.path.join(ann_dir, '*' + ann_ext))
+        img_pths = glob.glob(os.path.join(img_dir, '*' + img_ext))
+
+        assert len(ann_pths) == len(img_pths)
+
+        self.ann_pths = sorted(ann_pths, key=lambda x: x.split('/')[-1])
+        self.img_pths = sorted(img_pths, key=lambda x: x.split('/')[-1])
+
+        check_loc = np.random.randint(len(ann_pths))
+        assert self.ann_pths[check_loc].split('/')[-1].replace(ann_ext, '') == self.img_pths[check_loc].split('/')[-1].replace(img_ext, '')
+
+        self.theta = theta
+        self.sigma = sigma
+
+        self.x_ratio = end_size[0]/size[0]
+        self.y_ratio = end_size[1]/size[1]
+
+        self.img_size = size
+
+        self.end_size = end_size
+        self.stride = stride
     
     def __len__(self):
-        return len(self.data)
-    
+        return len(self.ann_pths)
+
     '''
         Keypoints COCO (augemented w/ neck):
-        0: nose	   		1: neck         2: l eye    3: r eye	4: l ear	  5: r ear
-        6: l shoulder	7: r shoulder	8: l elbow	9: r elbow  10: l wrist	 11: r wrist		
-        12: l hip	   13: r hip       14: l knee  15: r knee   16: l ankle	 17: r ankle
+        0:nose	   		1: neck         2: l eye        3: r eye	 4: l ear	  5: r ear        
+        6: l shoulder	7: r shoulder	8: l elbow	    9: r elbow  10: l wrist  11: r wrist
+        12: l hip	   13: r hip       14: l knee      15: r knee   16: l ankle	 17: r ankle
     '''
-    def __getitem__(self, idx):
-        img_path, ann_path = self.data[idx]
-        img = io.imread(img_path)
-        curr_json_f = open(ann_path, 'r')
-        anns = json.load(curr_json_f)
+    def __getitem__(self, index):
+        _jf = open(self.ann_pths[index], 'r')
+        curr_ann = json.load(_jf)
+        curr_img = np.array(cv2.imread(self.img_pths[index]), dtype=np.float32)
 
-        if 'img' in self.transforms.keys():
-            img = self.transforms['img'](img)
+        max_side = max(curr_img.shape[:2])
+
+        y_pad = int((max_side - curr_img.shape[0])/2)
+        x_pad = int((max_side - curr_img.shape[1])/2)
+
+        x_orig = curr_img.shape[1]
+        y_orig = curr_img.shape[0]
+
+        # pad image to sqaure
+        curr_img = np.pad(curr_img, ((y_pad, y_pad), (x_pad, x_pad), (0,0)), mode='constant', constant_values=0)
+
+        curr_img = cv2.resize(curr_img, self.img_size)
+
+        scale_x = curr_img.shape[1]/x_orig
+        scale_y = curr_img.shape[0]/y_orig
+
+        # print(x_orig, curr_img.shape[1], y_orig, curr_img.shape[0])
+        # print(scale_x, scale_y)
         
-        
-        intermediate_reprs = []
-        for ann in anns:
-            curr_kp_repr = {}
-            for i in range(0, len(ann['keypoints']), 3):
-                kp = i/3
-                if ann['keypoints'][i+2] != 0:
-                    curr_kp_repr[kp] = np.array(ann['keypoints'][i:i+2])
+        ann_list = []
+        for ann_idx in range(len(curr_ann)):
+            kpts = curr_ann[ann_idx]['keypoints']
+
+            point_dict = {}
+            limb_dict = {}
+            for i in range(0, len(kpts), 3):
+                kp = kpts[i:i+3]
+                kp_idx = i if i == 0 else (i//3)+1
+                if kp[2] > 0:
+                    point_dict[kp_idx] = [kp[0] * scale_x, kp[1] * scale_y, kp[2]]
                 else:
-                    curr_kp_repr[kp] = None
-            intermediate_reprs.append(curr_kp_repr)
-
-            for k in sorted(curr_kp_repr.keys(), reverse=True):
-                if i/3 > 0:
-                    curr_kp_repr[i/3+1] = curr_kp_repr[i/3]
+                    point_dict[kp_idx] = []
             
-            if curr_kp_repr[6] is not None and curr_kp_repr[7] is not None:
-                curr_kp_repr[1] = (curr_kp_repr[6] + curr_kp_repr[7])/2
-                curr_kp_repr[1] = curr_kp_repr[1].astype(np.int)
+            ls = point_dict[6]
+            rs = point_dict[7]
+
+            if len(ls) != 0 and len(rs) != 0 and ls[2] > 0 and rs[2] > 0:
+                    indicator = 2 if ls[2] == 2 and rs[2] == 2 else 1
+                    point_dict[1] = [(ls[0] + rs[0])//2, (ls[1] + rs[1])//2, indicator]
             else:
-                curr_kp_repr[1] = None
+                point_dict[1] = []
+            
+            i = 1 
+            for limb in limb_set:
+                j1, j2 = limb
+                if len(point_dict[j1]) != 0 and len(point_dict[j2]) != 0:
+                    limb_dict[limb] = [point_dict[j1], point_dict[j2]]
+                else:
+                    limb_dict[limb] = []
+                i+= 1
+            
+            ann_list.append([point_dict, limb_dict])
 
         kp_maps = {}
-        for kps in intermediate_reprs:
-            for i in kps.keys():
-                if i not in kp_maps.keys():
-                    kp_maps[i] = np.zeros((img.size(1), img.size(2)))
-                if kps[i] is not None:
-                    kp_maps[i] = gaussianOnPt(kp_maps[i], kps[i], 1)
-        
-        paf_maps = {}
-        paf_counts = {}
-        for kps in intermediate_reprs:
-            for limb in limb_set:
-                if limb not in paf_maps.keys():
-                    paf_maps[limb] = np.zeros((img.size(1), img.size(2), 2))
-                    paf_counts[limb] = np.zeros((img.size(1), img.size(2), 2))
-                f,t = limb
-                pt1 = kps[f]
-                pt2 = kps[t]
-                if pt1 is not None and pt2 is not None:
-                    paf_maps[limb] = pafOnPt(paf_maps[limb], paf_counts[limb], pt1, pt2, 2)
-                    paf_counts[limb] += 1
-        
-        for limb in limb_set:
-            paf_maps[limb][paf_counts[limb] != 0] /= paf_counts[limb][paf_counts[limb] != 0]
-        curr_json_f.close()
+        limb_maps = {}
+        counts = {}
+        for ann in ann_list:
+            curr_point_dict, curr_limb_dict = ann
+            for i in range(17):
+                curr_kp = curr_point_dict[i]
+                if i not in kp_maps:
+                    kp_maps[i] = np.zeros(self.end_size)
+                if len(curr_kp) != 0:
+                    kp_maps[i] = put_gaussian(curr_kp, kp_maps[i], self.sigma, self.stride)
+            
+            for limb in curr_limb_dict.keys():
+                curr_limb = curr_limb_dict[limb]
+            
+                if limb not in limb_maps.keys():
+                    limb_maps[limb] = np.zeros((self.end_size[0], self.end_size[1], 2))
+                    counts[limb] = 0
+                if len(curr_limb) != 0:
+                    curr_lm, curr_cnt = put_paf(curr_limb[0], curr_limb[1], limb_maps[limb], self.theta, self.stride)
+                    limb_maps[limb] = curr_lm
+                    counts[limb] += curr_cnt
 
-        cat_kps = [torch.from_numpy(x[1][None,:,:]) for x in sorted(kp_maps.items())]
-        kp_maps = torch.cat(cat_kps, 0)
+        kp_arr = [torch.FloatTensor(x).unsqueeze(0) for _,x in sorted(kp_maps.items(), key=lambda x: x[0])]
+        limb_arr = []
+        key_set = sorted(limb_maps.keys(), key = lambda x: x[0])
+        for limb_k in key_set:
+            if counts[limb_k] != 0:
+                limb_arr.append(torch.from_numpy(limb_maps[limb_k]/counts[limb_k]).unsqueeze(0))
+            else:
+                limb_arr.append(torch.from_numpy(limb_maps[limb_k]).unsqueeze(0))
 
-        cat_pafs = [torch.from_numpy(x[1]).permute(2, 0, 1) for x in sorted(paf_maps.items())]
-        paf_maps = torch.cat(cat_pafs, 0)
+        _jf.close()
+        return curr_img, torch.cat(kp_arr, 0), torch.cat(limb_arr, 0)
 
-        print(kp_maps.size())
-        print(paf_maps.size())
+base_path = '/home/shared/workspace/coco_keypoints'
+cocoloader_test = CocoPoseDataset(os.path.join(base_path, 'annotations'), os.path.join(base_path, 'images'))
 
-        for key in self.transforms.keys():
-            if key == 'img':
-                img = self.transforms['img'](img)
-            if key == 'kp':
-                kp_maps = self.transforms['kp'](kp_maps)
-            if key == 'paf':
-                paf_maps = self.transforms['paf'](paf_maps)
-        return (img, kp_maps, paf_maps)
+# idx = 24399
+idx = np.random.randint(len(cocoloader_test))
+print(cocoloader_test[idx][1].size())
+print(cocoloader_test[idx][2].size())
--- rtpose_trainer.py
+++ rtpose_trainer.py
@@ -7,6 +7,7 @@ from rtpose import rtpose_model
 import torch.nn as nn
 import torch.optim as optim
 
+from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 
 def gen_loss(intermediate_signals, kp_gt, paf_gt, device, criterion):
@@ -33,11 +34,12 @@ def main():
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
     model = rtpose_model(freeze_vgg=False)
-    model = model.to(device)
+    # model = model.to(device)
 
     base_path = '/home/shared/workspace/coco_keypoints'
-    trans = {'img': transforms.Compose([transforms.ToTensor()])}
-    cocodset = CocoPoseDataset(os.path.join(base_path, 'images'), os.path.join(base_path, 'annotations'), transforms=trans)
+    cocoset = CocoPoseDataset(os.path.join(base_path, 'annotations'), os.path.join(base_path, 'images'))
+    cocoloader = DataLoader(cocoset, batch_size=4, shuffle=True, num_workers=4)
+
 
     epochs = 5
 
@@ -48,12 +50,14 @@ def main():
     opt = optim.SGD(train_params, lr=1)
 
     for e in range(epochs):
-        for i, data in enumerate(cocodset):
+        for i, data in enumerate(cocoloader):
             img, kp_gt, paf_gt = data
 
-            img = img.to(device)
-            kp_gt = kp_gt.to(device)
-            paf_gt = paf_gt.to(device)
+            # TODO: Need to set list of stages to self in the model, so that *.to(device) works
+
+            # img = img.to(device)
+            # kp_gt = kp_gt.to(device)
+            # paf_gt = paf_gt.to(device)
 
             last_layer, intermediate_signals = model(img)
 
@@ -64,7 +68,7 @@ def main():
             opt.step()
 
             if i % 100 == 0:
-                print('Epoch [%d/%d], Batch [%d/%d], Total Loss %f' % (e, epochs, i, len(cocodset), curr_loss.item()))
+                print('Epoch [%d/%d], Batch [%d/%d], Total Loss %f' % (e, epochs, i, len(cocoset), curr_loss.item()))
 
 if __name__ == '__main__':
     main()
