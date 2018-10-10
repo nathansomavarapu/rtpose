630c1550cbc65adb04ae60fc0ded9fd3e6b3f137
nsom <somavarapun@gmail.com>
2018-10-10 01:59:24 +0000

    Added some NMS, model liley still needs more training.

--- rtpose_eval.py
+++ rtpose_eval.py
@@ -7,6 +7,9 @@ import cv2
 import numpy as np
 import os
 
+from scipy.ndimage.filters import maximum_filter
+from scipy.ndimage.morphology import generate_binary_structure, binary_erosion
+
 in_size=(368,368)
 limb_set = [(0,1), (0,2), (0,3), (2,4), (3,5), (1,6), (1,7), (6,8), (7,9), (8,10), (9,11), (1,12), (1,13), (12,14), (13,15), (14,16), (15,17)]
 
@@ -20,31 +23,47 @@ if os.path.exists('rtpose.pt'):
 
 model.eval()
 
-curr_img = cv2.imread('test.jpg')
+with torch.no_grad():
+    curr_img = cv2.imread('test.jpg')
 
-max_side = max(curr_img.shape[:2])
+    max_side = max(curr_img.shape[:2])
 
-y_pad = int((max_side - curr_img.shape[0])/2)
-x_pad = int((max_side - curr_img.shape[1])/2)
+    y_pad = int((max_side - curr_img.shape[0])/2)
+    x_pad = int((max_side - curr_img.shape[1])/2)
 
-curr_img = np.pad(curr_img, ((y_pad, y_pad), (x_pad, x_pad), (0,0)), mode='constant', constant_values=0)
+    curr_img = np.pad(curr_img, ((y_pad, y_pad), (x_pad, x_pad), (0,0)), mode='constant', constant_values=0)
 
-x_orig = curr_img.shape[1]
-y_orig = curr_img.shape[0]
+    x_orig = curr_img.shape[1]
+    y_orig = curr_img.shape[0]
 
-curr_img = cv2.resize(curr_img, in_size)
+    curr_img = cv2.resize(curr_img, in_size)
 
-curr_img = cv2.cvtColor(curr_img, cv2.COLOR_BGR2RGB)
-curr_img = curr_img.astype(np.float32)
-curr_img = curr_img/255.0
+    curr_img = cv2.cvtColor(curr_img, cv2.COLOR_BGR2RGB)
+    curr_img = curr_img.astype(np.float32)
+    curr_img = curr_img/255.0
 
-scale_x = curr_img.shape[1]/x_orig
-scale_y = curr_img.shape[0]/y_orig
+    scale_x = curr_img.shape[1]/x_orig
+    scale_y = curr_img.shape[0]/y_orig
 
-curr_img = torch.from_numpy(curr_img.transpose(2,0,1)).unsqueeze(0)
-curr_img = curr_img.to(device)
-last_layer, _ = model(curr_img)
+    curr_img = torch.from_numpy(curr_img.transpose(2,0,1)).unsqueeze(0)
+    curr_img = curr_img.to(device)
+    last_layer, _ = model(curr_img)
 
-kp, paf = last_layer
+    kp, paf = last_layer[0][0], last_layer[1][0]
+    kp = kp.cpu().data.numpy()
+    paf = paf.cpu().data.numpy()
 
-for i in range()
+    fprint = generate_binary_structure(2,2)
+    for i in range(kp.shape[0]):
+        local_max = maximum_filter(kp[i], footprint=fprint) == kp[i]
+
+        background = (kp[i] == 0)
+
+        eroded_background = binary_erosion(background, structure=fprint, border_value=1)
+
+        detected_peaks = local_max ^ eroded_background
+
+        # TODO: This doesnt do anything fix it.
+        detected_peaks * (kp[i] > 1.9)
+
+        print(np.array(np.nonzero(detected_peaks)))
