0a05d93a2dab90bfa5a2424746842d1bf0ab6e9e
nsom <Somavarapun@gmail.com>
2018-11-27 19:49:35 +0000

    Added sample kps for viz.

--- rtpose_eval.py
+++ rtpose_eval.py
@@ -7,10 +7,14 @@ import cv2
 import numpy as np
 import os
 
-from scipy.ndimage.filters import maximum_filter
+from scipy import ndimage
+
+from time import time
 
 in_size=(368,368)
 limb_set = [(0,1), (0,2), (0,3), (2,4), (3,5), (1,6), (1,7), (6,8), (7,9), (8,10), (9,11), (1,12), (1,13), (12,14), (13,15), (14,16), (15,17)]
+colors = [(255,0,0), (255,128,0), (255,255,0), (255,255,0), (0,255,0), (0,255,0), (0,255,255), (0,255,255), (0,0,255), (0,0,255), (127,0,255), (127,0,255), 
+(127,0,255), (127,0,255), (127,0,255), (127,0,255), (127,0,255), (127,0,255)]
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -25,7 +29,8 @@ model = model.to(device)
 model.eval()
 
 with torch.no_grad():
-    curr_img = cv2.imread('test.jpeg')
+    # curr_img = cv2.imread('samples/test1.jpeg')
+    curr_img = cv2.imread('samples/test2.jpeg')
 
     max_side = max(curr_img.shape[:2])
 
@@ -39,37 +44,40 @@ with torch.no_grad():
 
     curr_img = cv2.resize(curr_img, in_size)
 
+    curr_img_np = curr_img.copy()
+
     curr_img = cv2.cvtColor(curr_img, cv2.COLOR_BGR2RGB)
     curr_img = curr_img.astype(np.float32)
     curr_img = curr_img/255.0
 
-    scale_x = curr_img.shape[1]/x_orig
-    scale_y = curr_img.shape[0]/y_orig
-
     curr_img = torch.from_numpy(curr_img.transpose(2,0,1)).unsqueeze(0)
     curr_img = curr_img.to(device)
+
     last_layer, _ = model(curr_img)
 
     kps, pafs = last_layer[0][0], last_layer[1][0]
     kps = kps.cpu().data.numpy()
     pafs = pafs.cpu().data.numpy()
 
+    
+    scale_x = curr_img.size(3)/kps.shape[2]
+    scale_y = curr_img.size(2)/kps.shape[1]
+
+    centers = []
+
     for i, kp in enumerate(kps):
-        cv2.imwrite('kp_' + str(i) + '.png', maximum_filter(kp, size=(3,3)) * 255)
+        kp[kp < 0.5] = 0
+        lab_img, num_labs = ndimage.label(kp)
+        # _, _, _, curr_centers = ndimage.measurements.extrema(kp, lab_img, list(range(1, num_labs + 1)))
+        curr_centers = ndimage.measurements.center_of_mass(kp, lab_img, list(range(1, num_labs + 1)))
+        # print(curr_centers)
+        centers.append([( int(round(center[1] * scale_x)), int(round(center[0] * scale_y)) ) for center in curr_centers])
 
+        for c in centers[-1]:
+            cv2.circle(curr_img_np, c, 3, colors[i], -1)
 
+        # cv2.imwrite('kp_' + str(i) + '.png', kp * 255)
+    
+    cv2.imwrite('kp_draw.png', curr_img_np)
 
-    # fprint = generate_binary_structure(2,2)
-    # for i in range(kp.shape[0]):
-    #     local_max = maximum_filter(kp[i], footprint=fprint) == kp[i]
-
-    #     background = (kp[i] == 0)
-
-    #     eroded_background = binary_erosion(background, structure=fprint, border_value=1)
-
-    #     detected_peaks = local_max ^ eroded_background
-
-    #     # TODO: This doesnt do anything fix it.
-    #     detected_peaks * (kp[i] > 1.9)
-
-    #     print(np.array(np.nonzero(detected_peaks)))
+    # print(centers)
